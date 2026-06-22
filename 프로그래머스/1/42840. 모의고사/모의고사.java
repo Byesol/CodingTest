c58e19a9cfa28d2247adee6bf45fee314cde9e98
백승주 <bsj5967@naver.com>
2026-06-22 18:30:07 +0900

    [level 1] Title: 모의고사, Time: 0.45 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -4,46 +4,67 @@ class Solution {
         int[] p2 = {2, 1, 2, 3, 2, 4, 2, 5};
         int[] p3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
         
-        int score1 = 0, score2 = 0, score3 = 0;
-        int idx1 = 0, idx2 = 0, idx3 = 0; 
+        int score1 = 0;
+        int score2 = 0;
+        int score3 = 0;
+        
+        int index1 = 0; 
+        int index2 = 0; 
+        int index3 = 0; 
         
         for (int ans : answers) {
-            if (ans == p1[idx1]) score1++;
-            if (ans == p2[idx2]) score2++;
-            if (ans == p3[idx3]) score3++;
+            if (ans == p1[index1]) {
+                score1++;
+            }
+            if (ans == p2[index2]) {
+                score2++;
+            }
+            if (ans == p3[index3]) {
+                score3++;
+            }
             
-            idx1++;
-            idx2++;
-            idx3++;
+            index1++;
+            index2++;
+            index3++;
             
-            if (idx1 == 5) idx1 = 0;
-            if (idx2 == 8) idx2 = 0;
-            if (idx3 == 10) idx3 = 0;
+            if (index1 == 5) {
+                index1 = 0;
+            }
+            if (index2 == 8) {
+                index2 = 0;
+            }
+            if (index3 == 10) {
+                index3 = 0;
+            }
         }
         
-        // 4. 제일 높은 점수 찾기
         int maxScore = Math.max(score1, Math.max(score2, score3));
         
-        // 5. 1등이 몇 명인지 세기 (배열의 크기를 정하기 위해)
         int count = 0;
-        if (maxScore == score1) count++;
-        if (maxScore == score2) count++;
-        if (maxScore == score3) count++;
-        
-        int[] result = new int[count];
-        int resultIdx = 0;
-        
         if (maxScore == score1) {
-            result[resultIdx] = 1;
-            resultIdx++;
+            count++;
         }
         if (maxScore == score2) {
-            result[resultIdx] = 2;
-            resultIdx++;
+            count++;
         }
         if (maxScore == score3) {
-            result[resultIdx] = 3;
-            resultIdx++;
+            count++;
+        }
+        
+        int[] result = new int[count];
+        int resultIndex = 0;
+        
+        if (maxScore == score1) {
+            result[resultIndex] = 1;
+            resultIndex++;
+        }
+        if (maxScore == score2) {
+            result[resultIndex] = 2;
+            resultIndex++;
+        }
+        if (maxScore == score3) {
+            result[resultIndex] = 3;
+            resultIndex++;
         }
         
         return result;
